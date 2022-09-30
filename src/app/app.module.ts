bbe771089e1468ba529a7c7cc554436a8680d2d3
Elnur <ocaqquliyev8428@gmail.com>
2022-09-30 13:23:23 +0400

    First1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
+
 import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
